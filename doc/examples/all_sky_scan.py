09b9b62f7cb0f89d76a4445228f14702c5687e80
Stefan Coenders <stefan.coenders@tum.de>
2015-10-20 17:33:43 +0200

    adjust examples to work with latest changes

--- doc/examples/all_sky_scan.py
+++ doc/examples/all_sky_scan.py
@@ -9,17 +9,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # convert test statistic to a p-value for a given point
-pVal_func = lambda TS, dec: -np.log10(0.5 * (chi2(1).sf(TS) + chi2(1).cdf(-TS)))
+pVal_func = lambda TS, dec: -np.log10(0.5 * (chi2(len(llh.params)).sf(TS)
+                                             + chi2(len(llh.params)).cdf(-TS)))
 
 if __name__=="__main__":
 
     # init the llh class
-    llh = data.multi_init(4, 1000, 10000, ncpu=4)
+    llh = data.multi_init(4, 1000, 100000, ncpu=1, energy=True)
 
     print(llh)
 
     # iterator of all-sky scan with follow up scans of most interesting points
-    for i, (scan, hotspot) in enumerate(llh.all_sky_scan(nside=32,
+    for i, (scan, hotspot) in enumerate(llh.all_sky_scan(nside=16,
                                                          pVal=pVal_func,
                                                          decRange=np.radians([-90., 90.]))):
         if i > 0:
@@ -27,15 +28,32 @@ if __name__=="__main__":
             break
 
     # plot results
-    hp.mollview(scan["pVal"], min=0., cmap=plt.cm.afmhot)
+    hp.mollview(scan["pVal"], min=0., cmap=plt.cm.afmhot, rot=[-180., 0., 0.])
     if isinstance(llh, MultiPointSourceLLH):
         for llh in llh._sams.itervalues():
             hp.projscatter(np.degrees(llh.exp["ra"]),
                            np.degrees(np.arcsin(llh.exp["sinDec"])),
+                           rot=[-180., 0., 0.],
                            lonlat=True, marker="x", color=plt.gca()._get_lines.color_cycle.next())
     else:
         hp.projscatter(np.degrees(llh.exp["ra"]),
                        np.degrees(np.arcsin(llh.exp["sinDec"])),
+                       rot=[-180., 0., 0.],
                        lonlat=True, marker="x", color="red")
+
     plt.show()
+
+    for key in llh.params:
+        eps = 0.1
+        q = scan[key]
+        if not key == "nsources":
+            q = np.ma.masked_array(q)
+            q.mask = ~(scan["nsources"]
+                       > np.percentile(scan["nsources"][scan["nsources"] > 0],
+                                       eps))
+        hp.mollview(q, unit=key, cmap=plt.cm.cubehelix,
+                    rot=[-180., 0., 0.],
+                    min=min(0., np.percentile(scan[key], eps)),
+                    max=np.percentile(scan[key], 100. - eps))
+        plt.show()
 
--- doc/examples/data.py
+++ doc/examples/data.py
@@ -42,18 +42,19 @@ def MC(N=1000):
                                  ("trueRa", np.float), ("trueDec", np.float),
                                  ("trueE", np.float), ("ow", np.float)])
 
-    arr["ra"] = np.random.uniform(0., 2.*np.pi, N)
-    arr["sinDec"] = np.random.uniform(-1., 1., N)
-    arr["sigma"] = np.random.lognormal(mean=log_mean, sigma=log_sig, size=N)
-    arr["logE"] = logE_mean + logE_sig * np.random.normal(size=N)
+    # true information
 
-    eta = np.random.uniform(0., 2.*np.pi, N)
-    arr["trueRa"] = arr["ra"] + np.cos(eta) * arr["sigma"] / np.sqrt(1. - arr["sinDec"]**2)
-    arr["trueDec"] = np.arcsin(arr["sinDec"]) + np.sin(eta) * arr["sigma"]
-    arr["trueRa"] = np.arccos(np.cos(arr["trueRa"]))
-    arr["trueDec"] = np.arcsin(np.sin(arr["trueDec"]))
+    arr["trueRa"] = np.random.uniform(0., 2.*np.pi, N)
+    arr["trueDec"] = np.arcsin(np.random.uniform(-1., 1., N))
     arr["trueE"] = np.random.lognormal(mean=logE_mean, sigma=logE_sig, size=N)
-    arr["ow"] = 1./N
+    arr["ow"] = arr["trueE"]
+    arr["ow"] /= arr["ow"].sum()
+
+    eta = np.random.uniform(0., 2.*np.pi, len(arr))
+    arr["sigma"] = np.random.lognormal(mean=log_mean, sigma=log_sig, size=N)
+    arr["ra"] = arr["trueRa"] + np.cos(eta) * arr["sigma"] / np.cos(arr["trueDec"])
+    arr["sinDec"] = np.sin(arr["trueDec"] + np.sin(eta) * arr["sigma"])
+    arr["logE"] = np.log10(arr["trueE"]) + np.random.normal(size=len(arr)) #/ 4
 
     return arr
 
@@ -62,34 +63,36 @@ def init(Nexp, NMC, energy=False, **kwargs):
     arr_mc = MC(NMC)
 
     if energy:
-        llh_model = EnergyLLH(sinDec_bins=max(2, Nexp // 10),
+        llh_model = EnergyLLH(sinDec_bins=max(3, Nexp // 200),
                               sinDec_range=[-1., 1.],
-                              twodim_bins=max(3, Nexp // 100),
+                              twodim_bins=max(3, Nexp // 200),
                               twodim_range=[[0.9 * min(arr_exp["logE"].min(),
                                                        arr_mc["logE"].min()),
                                              1.1 * max(arr_exp["logE"].max(),
                                                        arr_exp["logE"].max())],
                                              [-1., 1.]])
     else:
-        llh_model = ClassicLLH(sinDec_bins=max(2, Nexp // 10),
+        llh_model = ClassicLLH(sinDec_bins=max(3, Nexp // 200),
                                sinDec_range=[-1., 1.])
 
     llh = PointSourceLLH(arr_exp, arr_mc, 365., llh_model=llh_model,
                          mode="all", hemispheres=dict(Full=[-np.inf, np.inf]),
-                         rho_nsource_bounds=(-0.8, 0.8),
+                         rho_nsource_bounds=(-0.8, 0.8) if not energy else (0., 0.8),
                          seed=np.random.randint(2**32),
                          **kwargs)
 
     return llh
 
 def multi_init(n, Nexp, NMC, **kwargs):
+    energy = kwargs.pop("energy", False)
+
     llh = MultiPointSourceLLH(hemispheres=dict(Full=[-np.inf, np.inf]),
-                              rho_nsource_bounds=(-0.8, 0.8),
+                              rho_nsource_bounds=(-0.8, 0.8) if not energy else (0., 0.8),
                               seed=np.random.randint(2**32),
                               **kwargs)
 
     for i in xrange(n):
-        llh.add_sample(str(i), init(Nexp, NMC, **kwargs))
+        llh.add_sample(str(i), init(Nexp, NMC, energy=energy))
 
     return llh
 
